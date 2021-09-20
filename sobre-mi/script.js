9f2e69103fe5886724d6215aae02d43d8a786ac8
Jonatan-13 <85076613+Jonatan-13@users.noreply.github.com>
2021-09-19 19:22:01 -0500

    Update script.js

--- sobre-mi/script.js
+++ sobre-mi/script.js
@@ -28,43 +28,43 @@ addEventListener("resize", () => {
 //container
 let info = [
     {
-        h3: "Título 1",
-        p: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam iure soluta cupiditate ducimus fugit sapiente ullam autem atque veritatis molestiae."
+        h3: "Mis talentos",
+        p: "Yo, desde que empecé la primaria, me he destacado en la asignatura de matemáticas y también en algunos deportes como el atletismo y el ultimate frisbee."
     },{
-        h3: "Título 2",
-        p: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam iure soluta cupiditate ducimus fugit sapiente ullam autem atque veritatis molestiae."
+        h3: "Mis hobbies",
+        p: "Hace unos años me encantaba leer libros de ficción y más que todo aquellos que pertenecen a sagas. Sin embargo, hoy me enfoco en libros de los cuales pueda aprender algo de algún area de conocimiento o algo que sea útil para la vida. Tambien me gusta escuchar música y hacer Yoga."
     },{
-        h3: "Título 3",
-        p: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam iure soluta cupiditate ducimus fugit sapiente ullam autem atque veritatis molestiae."
+        h3: "¿Qué me gustaría mejorar o aprender?",
+        p: "Siento que a veces no empleo el tiempo necesario para ciertas actividades a las que debería dedicarme más, así que debería tener un mejor manejo del tiempo y ser un poco más responsable con ese tipo de cosas."
     },{
-        h3: "Título 4",
-        p: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam iure soluta cupiditate ducimus fugit sapiente ullam autem atque veritatis molestiae."
+        h3: "Habilidades y CAS",
+        p: "Creo que con mis habilidades relacionadas al uso de la lógica y las matemáticas yo podría ayudar a personas a mejorar en estas habilidades. Además, al tener un gran gusto por el deporte también podría participar en algún proyecto relacionado a algún deporte o actividad deportiva."
     },{
-        h3: "Título",
-        p: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam iure soluta cupiditate ducimus fugit sapiente ullam autem atque veritatis molestiae."
+        h3: "¿Qué me gustaría hacer?",
+        p: "Me encantan las matemáticas, el uso de la lógica y el desarrollo de la creatividad, así que estaría dispuesto a realizar cualquier proyecto o actividad relacionada a estos conceptos, ya sea un club de matemáticas o la realización de algún programa, aplicación o página web."
     },{
-        h3: "Título",
-        p: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam iure soluta cupiditate ducimus fugit sapiente ullam autem atque veritatis molestiae."
+        h3: "Lo que me hace persona",
+        p: "Todo el tiempo estoy dispuesto a ayudar a los demás siempre que sea por una buena causa, soy generoso y decente, y siento que en todo momento debería comportarme según la moral por mi bien y el de los demás."
     },{
-        h3: "Título",
-        p: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam iure soluta cupiditate ducimus fugit sapiente ullam autem atque veritatis molestiae."
+        h3: "Momentos de reflexión",
+        p: "Ciertamente a lo largo de mi vida he cometido errores y a veces algunos que han afectado a las personas a mi alrededor, y es en esos momentos que reflexiono y tomo los comentarios de los demás para crecer como persona, finalmente aprendo de todos ellos."
     },{
-        h3: "Título",
-        p: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam iure soluta cupiditate ducimus fugit sapiente ullam autem atque veritatis molestiae."
+        h3: "¿Qué me apasiona?",
+        p: "Sinceramente, desde Septiembre del 2020 he adquirido un gigantesco amor por la programación y todo lo que se puede hacer con la tecnología, aún me sigue sorprendiendo y no pienso detener el aprendizaje sobre esta area."
     },{
-        h3: "Título",
-        p: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam iure soluta cupiditate ducimus fugit sapiente ullam autem atque veritatis molestiae."
+        h3: "Participación en la comunidad",
+        p: "En este momento no pertenezco a otra comunidad que no sea la del colegio, pero estoy dispuesto a unirme a cualquier asociación comunitaria con el propósito de ayudar al prójimo."
     },{
-        h3: "Título",
-        p: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam iure soluta cupiditate ducimus fugit sapiente ullam autem atque veritatis molestiae."
+        h3: "Mi gusto por la lógica",
+        p: "Siento un gran gusto por la lógica y las áreas en las que esta se puede aplicar, como las matemáticas y la programación, me pongo retos constanemente relacionados a esta áreas y sigo aprendiendo y mejorando en ellas."
     },{
-        h3: "Título",
-        p: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam iure soluta cupiditate ducimus fugit sapiente ullam autem atque veritatis molestiae."
+        h3: "Mi motivación",
+        p: "Desde el inicio de este 2021 he establecido algunas metas y he mejorado en ciertos aspectos de mi vida ya que ahora, en todo momento, pienso y actuo según lo que yo quiera y como quiera ser en el futuro, tratando de mejorar cada día y aprender de los errores que cometa."
     },{
-        h3: "Título",
-        p: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam iure soluta cupiditate ducimus fugit sapiente ullam autem atque veritatis molestiae."
-    },
-]
+        h3: "Mis cualidades",
+        p: "Yo me caracterizo principalmente por mi tranquilidad frente diversas situaciones, por mi aptitud para ayudar a los demás y por la capacidad de comprender los diferentes puntos de vista que puede llegar a tener la gente."
+    }
+];
 
 
 let container = document.querySelector(".container");
@@ -86,6 +86,19 @@ for (let i=0; i<12; i++) {
     <p>${info[i].p}</p>
     `;
 }
+for (let d of datos) {
+    let dH3 = d.firstElementChild;
+    let dP = dH3.nextElementSibling;
+    dH3.classList.add("center");
+    dP.classList.add("hidden");
+    d.style.cursor = "pointer";
+    d.addEventListener("click", () => {
+        dH3.classList.toggle("left-top");
+        dH3.classList.toggle("center");
+        dP.classList.toggle("visible");
+        dP.classList.toggle("hidden");
+    });
+}
 
 // let rows = document.querySelectorAll(".row");
 // for (let row of rows) {
@@ -103,3 +116,63 @@ for (let i=0; i<12; i++) {
 //         count += 1;
 //     }
 // }
+
+
+
+let change = (child, delay, direction) => {
+    for (let i=1; i<=4; i++) {
+        let d = document.querySelector(`.row:nth-child(${child}) .dato:nth-child(${i})`);
+        let count = 1;
+        let order = i;
+        setTimeout(() => {
+            var changeOrder = setInterval(() => {
+                if (direction === "left") {
+                    if (order-count > 0) {
+                        d.style.order = order-count;
+                        count++;
+                    }
+                    else {
+                        order = 4;
+                        d.style.order = order;
+                        count = 1;
+                    }
+                }
+                else if (direction === "right") {
+                    if (order+count < 5) {
+                        d.style.order = order+count;
+                        count++;
+                    }
+                    else {
+                        order = 1;
+                        d.style.order = order;
+                        count = 1;
+                    }
+                }
+            }, 7500);
+        }, delay);
+    }
+}
+change(1, 0, "left");
+change(2, 0, "right");
+change(3, 3000, "left");
+let rows = document.querySelectorAll(".row");
+for (let row of rows) {
+    row.addEventListener("mouseenter", (e) => {
+        let ds = row.children;
+        for (let d of ds) {
+            d.style.animationPlayState = "paused";
+        }
+    });
+    row.addEventListener("mouseleave", (e) => {
+        let ds = row.children;
+        for (let d of ds) {
+            let dH3 = d.firstElementChild;
+            let dP = dH3.nextElementSibling;
+            dH3.classList.remove("left-top");
+            dH3.classList.add("center");
+            dP.classList.remove("visible");
+            dP.classList.add("hidden");
+            d.style.animationPlayState = "running";
+        }
+    });
+}
